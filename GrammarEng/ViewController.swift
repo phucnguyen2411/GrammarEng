3c99d1e55676d191ff97a85cd1eae947d2c15e2d
Phuc .Ng <phucnguyen.2411@gmail.com>
2017-10-28 00:46:15 +0700

    #ADD: Silde menu and table view

--- GrammarEng/ViewController.swift
+++ GrammarEng/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.addSlideMenuButton()
     }
 
     override func didReceiveMemoryWarning() {
